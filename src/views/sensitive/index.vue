f8dbef01b1c5122c5f38eb918a5160e83cb66430
gk07 <gk_520@hotmail.com>
2022-06-26 21:07:03 +0800

    add filter for sensitive/index.vue

--- src/views/sensitive/index.vue
+++ src/views/sensitive/index.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="app-container sensitive-page">
-    <div>
-      <el-button
-        style="margin: 0 0 20px 20px"
-        type="primary"
-      >屏蔽管理</el-button>
-      <el-button
-        style="margin: 0 0 20px 20px"
-        type="primary"
-      >配置管理</el-button>
-      <el-button
-        style="margin: 0 0 20px 20px"
-        type="primary"
-      >规则管理</el-button>
+
+    <div class="filter-container">
+      <el-select v-model="listQuery.version" style="width: 90px" class="filter-item" @change="handleFilter">
+        <el-option v-for="item in versionOptions" :key="item" :label="item" :value="item" />
+      </el-select>
+      <el-select v-model="listQuery.product" style="width: 90px" class="filter-item" @change="handleFilter">
+        <el-option v-for="item in productOptions" :key="item" :label="item" :value="item" />
+      </el-select>
+      <el-button class="filter-item" type="primary" icon="el-icon-setting">屏蔽管理</el-button>
+      <el-button class="filter-item" type="primary" icon="el-icon-setting">规则管理</el-button>
+      <el-button class="filter-item" type="primary" icon="el-icon-setting">配置管理</el-button>
+      <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
+        Export
+      </el-button>
     </div>
 
-    <el-table :data="tableData" :row-class-name="tableRowClassName" fit>
+    <el-table :data="list" :row-class-name="tableRowClassName" fit>
       <el-table-column
         prop="group_name"
         label="分组"
@@ -42,41 +43,64 @@
 </template>
 
 <script>
+import waves from '@/directive/waves' // waves directive
+import { parseTime } from '@/utils'
+
 export default {
   name: 'Sensitive',
+  directives: { waves },
   props: {},
   data() {
     return {
+      list: null,
+      listQuery: {
+        page: 1,
+        limit: 20,
+        version: '23A',
+        product: '4G'
+      },
+      listLoading: true,
+      downloadLoading: false,
       tableData: [
         {
           group_name: 'Group1',
           rule1: 10,
           rule2: 0,
           rule3: 20,
-          rule4: 10
+          rule4: 10,
+          version: '23A',
+          product: '4G'
         },
         {
           group_name: 'Group2',
           rule1: 0,
           rule2: 0,
           rule3: 0,
-          rule4: 0
+          rule4: 0,
+          version: '23A',
+          product: '4G'
         },
         {
           group_name: 'Group3',
           rule1: 10,
           rule2: 0,
           rule3: 20,
-          rule4: 10
+          rule4: 10,
+          version: '23A',
+          product: '4G'
         },
         {
           group_name: 'Group4',
           rule1: 10,
           rule2: 0,
           rule3: 20,
-          rule4: 10
+          rule4: 10,
+          version: '23A',
+          product: '4G'
         }
-      ]
+      ],
+      versionOptions: ['23B', '23A', '22B', '22A'],
+      productOptions: ['4G', '5G']
     }
   },
   computed: {
@@ -93,7 +117,9 @@ export default {
       return rules
     }
   },
-  created() {},
+  created() {
+    this.getList()
+  },
   mounted() {
   },
   methods: {
@@ -105,6 +131,44 @@ export default {
         }
       }
       return ''
+    },
+    getList() {
+      this.listLoading = true
+
+      // call api
+      console.log(this.listQuery)
+      this.list = this.tableData
+      setTimeout(() => {
+        this.listLoading = false
+      }, 1.5 * 1000)
+    },
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    handleDownload() {
+      this.downloadLoading = true
+      import('@/vendor/Export2Excel').then(excel => {
+        const tHeader = ['group_name', 'rule1', 'rule2', 'rule3', 'rule4', 'version', 'product']
+        const filterVal = ['group_name', 'rule1', 'rule2', 'rule3', 'rule4', 'version', 'product']
+        const data = this.formatJson(filterVal)
+        console.log(data)
+        excel.export_json_to_excel({
+          header: tHeader,
+          data,
+          filename: `whitebox-security-${this.listQuery.version}-${this.listQuery.product}`
+        })
+        this.downloadLoading = false
+      })
+    },
+    formatJson(filterVal) {
+      return this.list.map(v => filterVal.map(j => {
+        if (j === 'timestamp') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
     }
   }
 }
